4d2430b80893264a1f3cb69d7aacbd97e4611478
alejandro <tercerolasa@gmail.com>
2021-01-17 20:53:31 +0100

    "Segundo commit - el programa cuenta hasta 30"

--- src/actividad7GitIntelillij.java
+++ src/actividad7GitIntelillij.java
@@ -1,6 +1,6 @@
 public class actividad7GitIntelillij {
     public static void main(String[] args) {
-        for(int i = 0; i<= 20; i++){
+        for(int i = 0; i<= 30; i++){
             System.out.print(i+" ");
         }
     }
