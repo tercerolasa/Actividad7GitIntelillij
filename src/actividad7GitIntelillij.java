d604604c003a0a645f5cb0bf676adb77dfc91c7c
alejandro <tercerolasa@gmail.com>
2021-01-17 20:56:31 +0100

    "Tercer commit - el programa cuenta hasta 40"

--- src/actividad7GitIntelillij.java
+++ src/actividad7GitIntelillij.java
@@ -1,6 +1,6 @@
 public class actividad7GitIntelillij {
     public static void main(String[] args) {
-        for(int i = 0; i<= 30; i++){
+        for(int i = 0; i<= 40; i++){
             System.out.print(i+" ");
         }
     }
