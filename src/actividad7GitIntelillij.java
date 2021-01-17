fdf1eedd781ebd0d2f1f51cdea22422d54f78521
Alejandro <76227050+tercerolasa@users.noreply.github.com>
2021-01-17 21:03:17 +0100

    Commit - añadiendo linea al codigo

--- src/actividad7GitIntelillij.java
+++ src/actividad7GitIntelillij.java
@@ -2,6 +2,7 @@ public class actividad7GitIntelillij {
     public static void main(String[] args) {
         for(int i = 0; i<= 40; i++){
             System.out.print(i+" ");
+            System.out.print("He cambiado esta linea desde github para la rama testing);
         }
     }
 }
